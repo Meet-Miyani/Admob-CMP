4c4e8372200a8f64206c5848258733a202f46133
Meet Miyani <miyanimeet02@gmail.com>
2026-07-24 21:46:20 +0530

    feat: scaffold empty :admob-cmp-core module

---
+++ admob-cmp-core/build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidKmpLibrary)
+    alias(libs.plugins.mavenPublish)
+}
+
+kotlin {
+    explicitApi()
+
+    @OptIn(org.jetbrains.kotlin.gradle.dsl.abi.ExperimentalAbiValidation::class)
+    abiValidation { enabled.set(true) }
+
+    android {
+        namespace = "avinya.tech.yt.ads.core"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        withHostTest { isReturnDefaultValues = true }
+        compilerOptions { jvmTarget.set(JvmTarget.JVM_11) }
+    }
+
+    iosArm64()
+    iosSimulatorArm64()
+
+    sourceSets {
+        commonMain.dependencies {
+            implementation(libs.kotlinx.coroutines.core)
+        }
+        androidMain.dependencies {
+            implementation(libs.androidx.activity.compose)
+            implementation(libs.androidx.lifecycle.process)
+            implementation(libs.google.ads.mobile.sdk)
+            implementation(libs.google.user.messaging.platform)
+        }
+        commonTest.dependencies {
+            implementation(libs.kotlin.test)
+            implementation(libs.kotlinx.coroutines.test)
+        }
+        getByName("androidHostTest").dependencies {
+            implementation(libs.kotlin.test)
+            implementation(libs.kotlinx.coroutines.test)
+            implementation(libs.google.ads.mobile.sdk)
+            implementation("org.mockito:mockito-core:5.15.2")
+        }
+    }
+}
---
+++ admob-cmp-core/src/commonMain/kotlin/avinya/tech/yt/ads/CoreModulePlaceholder.kt
@@ -0,0 +1,3 @@
+package avinya.tech.yt.ads
+
+internal const val CORE_MODULE_PLACEHOLDER: String = "admob-cmp-core"
